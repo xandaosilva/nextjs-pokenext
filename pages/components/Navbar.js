f94e6d40054a3361ccd8923d19e16c98c20a6f70
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-20 04:39:48 -0300

    header and footer css

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../../styles/Navbar.module.css";
 
 export default function Navbar(){
     return(
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
                 <Image src="/images/pokeball.png" width="30" height="30" alt="PokeNext" />
                 <h1>PokeNext</h1>
             </div>
-            <ul>
+            <ul className={styles.link_items}>
                 <li><Link href="/"><a>Home</a></Link></li>
                 <li><Link href="/about"><a>About</a></Link></li>
             </ul>
