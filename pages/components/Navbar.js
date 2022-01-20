02cd1818f75119578668cd72a0bbe381ad98e5b0
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-20 03:43:09 -0300

    Creating Layout component

---
+++ pages/components/Navbar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <div>
+                <Image src="/images/pokeball.png" width="30" height="30" alt="PokeNext" />
+                <h1>PokeNext</h1>
+            </div>
+            <ul>
+                <li><Link href="/"><a>Home</a></Link></li>
+                <li><Link href="/about"><a>About</a></Link></li>
+            </ul>
+        </nav>
+    );
+}
