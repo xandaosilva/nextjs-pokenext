800e32a39a6a21291e41207926ee8df80d95e229
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-24 21:35:35 -0300

    conclusion pokemon page

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import styles from "../../styles/Pokemon.module.css";
+
 export const getStaticPaths = async() => {
     const maxPokemons = 251;
     const api = "https://pokeapi.co/api/v2/pokemon/";
@@ -29,6 +32,31 @@ export const getStaticProps = async(context) => {
 
 export default function Pokemon({pokemon}){
     return(
-        <p>{pokemon.name}</p>
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`} width="200" height="200" alt={pokemon.name} />
+            <div>
+                <h3>Number:</h3>
+                <p>#{pokemon.id}</p>
+            </div>
+            <div>
+                <h3>Type:</h3>
+                <div className={styles.types_container}>
+                    {pokemon.types.map((item, index) => (
+                        <span key={index} className={`${styles.type} ${styles["type_" + item.type.name]}`}>{item.type.name}</span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Height:</h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Weight:</h4>
+                    <p>{pokemon.weight / 10} Kg</p>
+                </div>
+            </div>
+        </div>
     );
 }
